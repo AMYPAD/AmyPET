dd028611cf9f8b9a9e496405ceae2f607856e466
Casper da Costa-Luis <casper.dcl@physics.org>
2020-11-28 05:34:15 +0000

    fix packaging

--- amypad/__main__.py
+++ amypad/__main__.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-import sys
+from .cli import main
 
-if "--ignore-gooey" not in sys.argv:
-    sys.argv.insert(1, "--ignore-gooey")  # must be before gooey import
-from .gui import main
-
-main(gui_mode=False)
+main()
