5cfec9d6330949ba5a43e3678e0ec18216cd9458
Casper da Costa-Luis <imaging@cdcl.ml>
2021-09-13 13:23:00 +0100

    test dirs

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,9 @@
+from os import getenv
+from pathlib import Path
+
 import pytest
+
+HOME = Path(getenv("DATA_ROOT", "~")).expanduser()
 
 
 @pytest.fixture(scope="session")
@@ -10,3 +15,28 @@ def nvml():
     except pynvml.NVMLError as exc:
         pytest.skip(str(exc))
     return pynvml
+
+
+@pytest.fixture(scope="session")
+def dimin():
+    trt = HOME / "amypad" / "DPUK" / "TRT"
+    if not trt.is_dir():
+        pytest.skip(
+            f"""Cannot find amypad/DPUK/TRT in
+${{DATA_ROOT:-~}} ({HOME}).
+"""
+        )
+    return trt
+
+
+@pytest.fixture(scope="session")
+def fimin(dimin):
+    mprage = (
+        dimin
+        / "NEW002_ODE_S02442"
+        / "TP0"
+        / "NEW002_PETMR_V1_00015_MR_images_MPRAGE_q-_MPRAGE_20200212145346_15.nii"
+    )
+    if not mprage.is_file():
+        pytest.skip("fimin not found")
+    return mprage
