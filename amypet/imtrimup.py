62afd31231582f5480fa4db8060b21e903fa2e38
Casper da Costa-Luis <imaging@cdcl.ml>
2021-10-08 03:44:05 +0100

    fix & expose imtrimup

--- amypet/imtrimup.py
+++ amypet/imtrimup.py
@@ -1,3 +1,20 @@
+"""Trimming and upscaling
+
+Usage:
+  imtrimup [options] <dyndir>
+
+Arguments:
+  <dyndir>  : Input folder containing dynamic scans [default: DirChooser]
+
+Options:
+  --glob PATTERN  : File matching pattern [default: *.nii*]
+  --scale FACTOR  : Sampling scale factor [default: 2:int]
+  --refim PATH  : Reference image [default: FileChooser]
+  --fcomment COMMENT  : Prefix to add to outputs
+  --no-memlim  : Whether to use more memory (faster)
+  --store-img-intrmd  : Whether to write output image files
+  --store-img  : Whether to write output image sum file
+"""
 import logging
 from pathlib import Path
 
@@ -10,35 +27,22 @@ def run(
     dyndir,
     glob="*.nii*",
     scale=2,
-    memlim=True,
     refim="",
     fcomment="",
+    no_memlim=False,
     store_img_intrmd=False,
     store_img=False,
 ):
-    """Trimming and upscaling
-    Args:
-      dyndir: folder containing dynamic scans
-      glob: matching pattern for folder contents
-    """
     dyndir = Path(dyndir)
     assert dyndir.is_dir()
     # get the file data
     fdyns = list(map(str, dyndir.glob(glob)))
+    log.info("found %d files", len(fdyns))
     log.info("sort the dynamic images and output the sorted file names and numpy array")
     imdyn = nimpa.niisort(fdyns)
     log.info("trim & upscale")
-    # NB: the first row is 'blank' due to the offset. TODO: check
-    dtrm = nimpa.imtrimup(
-        imdyn["files"][1:],
-        scale=scale,
-        memlim=memlim,
-        refim=refim,
-        fcomment=fcomment,
-        store_img_intrmd=store_img_intrmd,
-        store_img=store_img,
-    )
-    log.debug("trimmed and scaled images")
-    res = dtrm["fimi"]
-    log.debug("%r", res["im"].shape)
+    res = nimpa.imtrimup(imdyn["files"], scale=scale, memlim=not no_memlim, refim=refim or "",
+                         fcomment=fcomment or "", store_img_intrmd=store_img_intrmd,
+                         store_img=store_img)
+    log.debug("trimmed and scaled images, shape %r", res['im'].shape)
     return res
